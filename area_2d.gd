1b69f45a2ea8549c5a12486958d87f27f72d6bf8
SarahKinner <156657511+SarahKinner@users.noreply.github.com>
2026-02-17 00:19:53 -0700

    Im tweeking bruv

--- area_2d.gd
+++ area_2d.gd
@@ -1,16 +1,14 @@
 extends Area2D
 
-var dragging = false
+@export var food_item_scene: PackedScene  # Drag FoodItem scene here
 
 func _input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			dragging = event.pressed
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			var new_food = food_item_scene.instantiate()
+			get_parent().add_child(new_food)
+			new_food.global_position = global_position
 
-func _process(_delta):
-	if dragging:
-		global_position = get_global_mouse_position()
 
-func _on_body_entered(body):
-	if body.name == "Character":
-		queue_free()
+func _on_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
